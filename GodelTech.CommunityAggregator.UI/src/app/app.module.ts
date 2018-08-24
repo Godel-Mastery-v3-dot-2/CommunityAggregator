f2744e0fe8d77605ee02d9c7a234431e2ae49829
xDAndrew <xD.Andrew2012@yandex.by>
2018-08-24 17:58:29 +0300

    Implemented Login Pages

--- GodelTech.CommunityAggregator.UI/src/app/app.module.ts
+++ GodelTech.CommunityAggregator.UI/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleComponent } from './article/article.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes: Routes =[
+  { path: '', component: ArticleListComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, FooterComponent, ArticleListComponent, ArticleComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, NavBarComponent, FooterComponent, ArticleListComponent, ArticleComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
